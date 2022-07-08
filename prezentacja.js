081a75dd09d530cf42321021e8f5de0bc64e5660
Nikola Dziwusz <n.dziwusz@bonasoft.pl>
2022-07-08 12:55:50 +0200

    dodanie prezentacji

---
+++ prezentacja.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+// const name = prompt("what's your name ");
+
+// console.log(`hey there ${name}`);
+
+
+// const name = prompt("what's your name "); 
+// const hello = (name) => {
+//     console.log("hello", name)
+// };
+
+// hello(name);
